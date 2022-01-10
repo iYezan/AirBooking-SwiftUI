11ea904de3907929bd8cf16a0edd56f10c015990
Yezan Ahmed <hashir_hush@hotmail.co.uk>
2022-01-10 22:10:39 +0000

    Initial Commit

---
+++ AirlineBooking-SwiftUI/AirlineBooking_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirlineBooking_SwiftUIApp.swift
+//  AirlineBooking-SwiftUI
+//
+//  Created by iYezan on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AirlineBooking_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
